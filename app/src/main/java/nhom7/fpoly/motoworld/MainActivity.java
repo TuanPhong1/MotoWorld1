a805e43b147535b7ffb257b6d68fc61ab481bedf
MinhChien204 <cminh5344@gmail.com>
2023-11-06 13:47:59 +0700

    Initial commit

---
+++ app/src/main/java/nhom7/fpoly/motoworld/MainActivity.java
@@ -0,0 +1,14 @@
+    package nhom7.fpoly.motoworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
